06b5e172d8248cfea687067f859539f266d534ad
Devon Humes <devon.humes@gmail.com>
2012-01-14 19:13:47 -0700

    Changing something ELSE

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('home.views',
-    url(r'^$', 'index')
+urlpatterns = patterns('',
+    url(r'^$', 'cs462.home.views.index')
     # Examples:
     # url(r'^$', 'cs462.views.home', name='home'),
     # url(r'^cs462/', include('cs462.foo.urls')),
