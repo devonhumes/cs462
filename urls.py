064da0a0f1b0a4fbe56ef02368be3cab745ed26b
Devon Humes <devon.humes@gmail.com>
2012-01-14 18:44:16 -0700

    new url

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^/$', 'cs462first.views.index')
+urlpatterns = patterns('cs462first.views',
+    url(r'^/cs462/$', 'index')
     # Examples:
     # url(r'^$', 'cs462.views.home', name='home'),
     # url(r'^cs462/', include('cs462.foo.urls')),
